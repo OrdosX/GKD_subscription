eed2ad2d395c5ec20498e09f05edf48e3cadb025
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-03-26 18:20:27 +0800

    perf: RQrun 优化
    - 换掉 <<n
    - 去掉 activityIds 包名前缀

--- src/apps/cn.runningquotient.rq.ts
+++ src/apps/cn.runningquotient.rq.ts
@@ -15,24 +15,22 @@ export default defineGkdApp({
       rules: [
         {
           key: 0,
-          activityIds: 'cn.runningquotient.rq.page.common_page.MainActivity',
+          activityIds: '.page.common_page.MainActivity',
           matches: '[vid="iv_main_checkin_entry"]',
           snapshotUrls: 'https://i.gkd.li/i/16105502',
         },
         {
           preKeys: [0],
           key: 1,
-          activityIds:
-            'cn.runningquotient.rq.page.common_page.CommonContainerActivity',
+          activityIds: '.page.common_page.CommonContainerActivity',
           matches:
-            '@[visibleToUser=true][text="我知道了"] <<n [vid="common_h5_container"]',
+            '@Button[text="我知道了"][clickable=true] <2 View < [index=parent.childCount.minus(1)] <n WebView <<2 [vid="common_h5_container"]',
           snapshotUrls: 'https://i.gkd.li/i/16105938',
         },
         {
           preKeys: [1],
           key: 2,
-          activityIds:
-            'cn.runningquotient.rq.page.common_page.CommonContainerActivity',
+          activityIds: '.page.common_page.CommonContainerActivity',
           matches: '[vid="iv_setting_header_view_left"]',
           snapshotUrls: 'https://i.gkd.li/i/16105942',
         },
