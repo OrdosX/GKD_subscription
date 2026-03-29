1d846dd87895350f10e9a7044eb9e88ea84a596b
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-03-29 23:09:24 +0800

    feat: 住理生活 开屏广告 (#91)
    close #91

--- src/apps/com.whxinna.userplatform.ts
+++ src/apps/com.whxinna.userplatform.ts
@@ -5,6 +5,21 @@ export default defineGkdApp({
   name: '住理生活',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          matches:
+            '@[text=null][clickable=true][width<500][height<200] - * >2 [text*="广告"][text.length<10][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/26332230',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '通知提示-公告',
       rules: [
