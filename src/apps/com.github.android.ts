3589c9e5c20ebbf4eb7b3ab9538d89f03e8055dc
cjy0812 <cjy081219@Gmail.com>
2026-04-11 16:58:58 +0800

    feat: GitHub 功能类-PR自动展开详情

--- src/apps/com.github.android.ts
+++ src/apps/com.github.android.ts
@@ -20,5 +20,21 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '功能类-PR自动展开详情',
+      matchTime: 10000, // 只开头有那一个需要所以说为了节约资源加超时停止匹配
+      actionMaximum: 1,
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.main.MainActivity',
+          matches:
+            '@[text="阅读更多"][clickable=true][visibleToUser=true] <3 * <2 [vid="container"][childCount=2]',
+          snapshotUrls: 'https://i.gkd.li/i/26644420',
+          exampleUrls: 'https://e.gkd.li/1a5c6e13-1a77-400e-9e78-e164563824db',
+        },
+      ],
+    },
   ],
 });
